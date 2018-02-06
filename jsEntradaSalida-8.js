3b7db977513a32416657ac134301b8b5b4cd2585
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-06 19:00:01 -0300

    ejercicio 8
    tengo problema si esta puesto el resto bien, ya que el resto mucho no lo entiendo de su funcionamiento.
    pero se puede ejecutar, pero no se si esta bien ejecutado.

---
+++ jsEntradaSalida-8.js
@@ -0,0 +1,19 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resto entre el dividendo y el divisor.
+ej.: "El resto es 0 ."*/
+function SacarResto()
+{
+    var dividendo;
+    var divisor;
+
+    dividendo = document.getElementById('numeroDividendo').value;
+    divisor = document.getElementById('numeroDivisor').value;
+
+    dividendo = parseInt(dividendo);
+    divisor = parseInt(divisor);
+    
+    divisor = dividendo % divisor;
+
+    alert("el resto es: " + divisor);
+}
