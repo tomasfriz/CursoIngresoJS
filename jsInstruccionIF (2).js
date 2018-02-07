cf87ce5a0f6be704a3d6b017f75740fd5ad6d858
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-07 16:53:53 -0300

    ejercicio 2 con if
    funciona: perfecto
    se utilizo: if, alert, parseInt y document.getElementById.
    ejercicio terminado.

---
+++ jsInstruccionIF (2).js
@@ -0,0 +1,16 @@
+function Mostrar()
+{
+    var edad;
+    edad=17;
+
+    edad = document.getElementById("edad").value;
+    edad = parseInt(edad);
+
+    if(edad>17) 
+    {
+        alert("es mayor de edad");
+    }
+}
+
+
+
