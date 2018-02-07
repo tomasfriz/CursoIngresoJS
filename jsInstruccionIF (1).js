35ce451b3f0e88aac4385b1485111c7f185636fc
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-07 11:47:49 -0300

    ejercicio 1
    con if

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,16 @@
+function Mostrar()
+{
+//tomo la edad  
+    var edad;
+    edad=15;
+
+    edad = document.getElementById("edad").value;
+    edad = parseInt(edad);
+
+    if(edad==15)
+    {
+        alert("niña bonita");
+    }
+
+
+}//FIN DE LA FUNCIÓN
