1137c5bd8c5b110c569aa296677e605d834b453d
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-13 15:19:45 -0300

    ejercicio 3 de if
    funciona: perfecto.
    se uso: document.getElementById, parseInt, if, else y alert.
    estado: terminado.

--- jsInstruccionIF (3).js
+++ jsInstruccionIF (3).js
@@ -1,21 +1,18 @@
 function Mostrar()
 {
-    //la funcion solo funciona con numeros menor a 12 y con numeros mayor 17; los numeros entre el 12 y 17,inclusive 12 y 17, no funcionan.
-    
     var edad;
-    edad=12;
-    edad=17;
+    edad>=18;
 
     edad = document.getElementById("edad").value;
     edad = parseInt(edad);
-    
-    if (edad<12)
-      {
-          alert("es menor de edad");  
-    }
-    if (edad>17)
-     {
+
+    if(edad>=18)
+    {
         alert("es mayor de edad");
+    }
+    else
+    {
+        alert("es menor de edad");
     }
 
 }//FIN DE LA FUNCIÓN
