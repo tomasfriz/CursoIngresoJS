8fd23eeda9386652b4969098d4b7469086817202
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-13 15:30:10 -0300

    ejercicio 4 de if
    funciona: perfecto.
    se uso: document.getElementById, parseInt, if y alert.
    estado: terminado.

---
+++ jsInstruccionIF (4).js
@@ -0,0 +1,17 @@
+function Mostrar()
+{
+    var edad;
+    edad>=13;
+    edad<=17;
+
+    edad = document.getElementById("edad").value;
+    edad = parseInt(edad);
+
+    if(edad>=13 && edad<=17)
+    {
+        alert("es un adolescente");
+    }
+
+
+
+}//FIN DE LA FUNCIÓN
