0c9996ba6d473049c6163529a5446283af5b1c8e
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-14 11:31:07 -0300

    ejercicio 1 de switch
    funciona: perfecto.
    se uso: document.getElementById, switch, case, alert y break.
    estado: terminado.

---
+++ jsInstruccionSwitch(1).js
@@ -0,0 +1,23 @@
+function Mostrar()
+{
+    var mesDelAño = document.getElementById('mes').value;
+    switch(mesDelAño)
+    {
+        case "Enero":
+        alert("que comiences bien el año!!!");
+        break;
+
+        case "Marzo":
+        alert("a clase!!!");
+        break;
+
+        case "Julio":
+        alert("se vienen las vacaciones!!!");
+        break;
+
+        case "Diciembre":
+        alert("Felices fiestas!!!");
+        break;
+    }
+
+}//FIN DE LA FUNCIÓN
