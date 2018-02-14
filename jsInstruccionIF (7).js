a21c8f79f7c2ee699f2c449deb28c59e1a773f27
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-14 09:20:34 -0300

    ejercicio 7 de if
    funciona: perfecto.
    se uso: document.getElementById, if y alert.
    estado: terminado.

---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,19 @@
+function Mostrar()
+{
+    var estadoCivil;
+    var edad;
+
+    edad = document.getElementById("edad").value;
+    estadoCivil = document.getElementById("estadoCivil").value;
+    
+
+    if(estadoCivil != "Soltero")
+    {
+        if(edad <18)
+        {
+            alert("es muy pequeño para NO ser soltero");
+        }
+        
+    }
+
+}//FIN DE LA FUNCIÓN
