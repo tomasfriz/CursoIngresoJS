8c0ac089f25a1d45b4fbcf1d728ea0ef363cebe7
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-14 11:29:01 -0300

    ejercicio 10 de if
    no esta terminado
    tiene errores

--- jsInstruccionIF (10).js
+++ jsInstruccionIF (10).js
@@ -4,8 +4,22 @@ function Mostrar()
 	var numero;
 	
 	numero = document.getElementById("FromIngreso").value;
-	
+
 	numero = Math.floor((Math.random() * 10) + 1);
+	console.log(numero);
 
-
+	if(numero > 8)
+	{
+		alert("EXCELENTE");
+	}
+	if(numero >= 4)
+	{
+		alert("APROBO");
+	}
+	if(numero < 4)
+	{
+		alert("Vamos, la proxima se puede");
+	}
+	
+//hay errores.
 }//FIN DE LA FUNCIÓN
