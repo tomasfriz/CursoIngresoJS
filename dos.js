807c7f3151ae7cb007ea0b1bb32b45b5604f4c49
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-15 17:08:12 -0300

    ejercicio 2 del parcial 1
    funciona: perfecto.
    se uso: prompt, parseInt y document.getElementById.
    estado: terminado.

--- dos.js
+++ dos.js
@@ -2,15 +2,17 @@ function Mostrar()
 {
     var importe;
     var importeFinal;
+
     importe = prompt("ingrese precio");
+
     importe = parseInt(importe);
+
     importeFinal = importe * 0.21;
+
     importeFinal = parseInt(importeFinal);
+
     importeFinal = importe + importeFinal;
+    
     document.getElementById('importeFinal').value= importeFinal;
 
-
-
-
-  
 }
