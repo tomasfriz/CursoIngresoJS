1272a5dad31c533fa4f7c786dfdd08364931166a
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-15 15:29:15 -0300

    ejercicio 3 de entrada y salida
    funciona: perfecto.
    se usa: document.getElementById y alert.
    estado: terminado.

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+	var elNombre;
+
+	elNombre = document.getElementById("elNombre").value;
+	
 	alert("esto anda");
 }
 
