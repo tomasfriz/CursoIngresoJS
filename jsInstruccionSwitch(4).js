5ff2bf1c745d1a33e52e1518f6da6b0841fb4c9b
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-15 17:21:12 -0300

    ejercicio 4 de switch
    funciona: perfecto.
    se uso: switch, case, alert y break.
    estado: terminado.

---
+++ jsInstruccionSwitch(4).js
@@ -0,0 +1,44 @@
+function Mostrar()
+{
+    var mesDelAño = document.getElementById('mes').value;
+
+    switch(mesDelAño)
+    {
+        case "Enero":
+            alert("tiene 31 dias");
+            break;
+        case "Febrero":
+            alert("tiene 28 dias");
+            break;
+        case "Marzo":
+            alert("tiene 31 dias");
+            break;
+        case "Abril":
+            alert("tiene 30 dias");
+            break;
+        case "Mayo":
+            alert("tiene 31 dias");
+            break; 
+        case "Junio":
+            alert("tiene 30 dias");
+            break;
+        case "Julio":
+            alert("tiene 31 dias");
+            break; 
+        case "Agosto":
+            alert("tiene 31 dias");
+            break; 
+        case "Septiembre":
+            alert("tiene 30 dias");
+            break;
+        case "Octubre":
+            alert("tiene 31 dias");
+            break; 
+        case "Noviembre":
+            alert("tiene 30 dias");
+            break;
+        case "Diciembre":
+            alert("tiene 31 dias");
+    }
+
+}//FIN DE LA FUNCIÓN
