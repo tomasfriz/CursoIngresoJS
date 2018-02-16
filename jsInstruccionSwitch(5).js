33f4944272323c99440e32eaba73c6a12bb99bf4
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-16 16:52:11 -0300

    ejercicio 5 de switch
    funciona: perfecto.
    se uso: document.getElementById, parseInt, switch, case, alert y break.
    estado: terminado.

---
+++ jsInstruccionSwitch(5).js
@@ -0,0 +1,112 @@
+function Mostrar()
+{
+    var laHora = document.getElementById('hora').value;
+
+    laHora = parseInt(laHora);
+
+    switch(laHora)
+    {
+        case 1: 
+        {
+            break;
+        }
+        case 2:
+        {
+            break;
+        }
+        case 3:
+        {
+            break;
+        }
+        case 4:
+        {
+            break;
+        }
+        case 5:
+        {
+            break;
+        }
+        case 6:
+        {
+            break;
+        }
+        case 7:
+        {
+            alert("es de mañana");
+            break;
+        }  
+        case 8:
+        {
+            alert("es de mañana");
+            break;
+        }
+        case 9:
+        {
+            alert("es de mañana");
+            break;
+        }
+        case 10:
+        {
+            alert("es de mañana");
+            break;
+        }
+        case 11:
+        {
+            alert("es de mañana");
+            break;
+        }
+        case 12:
+        {
+            break;
+        }
+        case 13:
+        {
+            break;
+        }
+        case 14:
+        {
+            break;
+        }
+        case 15:
+        {
+            break;
+        }
+        case 16:
+        {
+            break;
+        }
+        case 17:
+        {
+            break;
+        }
+        case 18:
+        {
+            break;
+        }
+        case 19:
+        {
+            break;
+        }
+        case 20:
+        {
+            break;
+        }
+        case 21:
+        {
+            break;
+        }
+        case 22:
+        {
+            break;
+        }
+        case 23:
+        {
+            break;
+        }
+        case 24:
+        {
+            break;
+        }
+    }
+
+}//FIN DE LA FUNCIÓN
