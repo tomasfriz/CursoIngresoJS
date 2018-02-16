cd3b309db5425a590197027f7580f7923d7335a2
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-16 17:21:07 -0300

    ejercicio 6 de switch
    funciona: perfecto.
    se uso: document.getElementById, parseInt, if, else, switch, case, alert y break.
    estado: terminado.

---
+++ jsInstruccionSwitch(6).js
@@ -0,0 +1,139 @@
+function Mostrar()
+{
+    var laHora = document.getElementById('hora').value;
+
+    laHora = parseInt(laHora);
+
+    if(laHora)
+    {
+        switch(laHora)
+        {
+        case 1: 
+        {
+            alert("es de noche");
+            break;
+        }
+        case 2:
+        {
+            alert("es de noche");
+            break;
+        }
+        case 3:
+        {
+            alert("es de noche");
+            break;
+        }
+        case 4:
+        {
+            alert("es de noche");
+            break;
+        }
+        case 5:
+        {
+            alert("es de noche");
+            break;
+        }
+        case 6:
+        {
+            alert("es de noche");
+            break;
+        }
+        case 7:
+        {
+            alert("es de mañana");
+            break;
+        }  
+        case 8:
+        {
+            alert("es de mañana");
+            break;
+        }
+        case 9:
+        {
+            alert("es de mañana");
+            break;
+        }
+        case 10:
+        {
+            alert("es de mañana");
+            break;
+        }
+        case 11:
+        {
+            alert("es de mañana");
+            break;
+        }
+        case 12:
+        {
+            alert("es de tarde");
+            break;
+        }
+        case 13:
+        {
+            alert("es de tarde");
+            break;
+        }
+        case 14:
+        {
+            alert("es de tarde");
+            break;
+        }
+        case 15:
+        {
+            alert("es de tarde");
+            break;
+        }
+        case 16:
+        {
+            alert("es de tarde");
+            break;
+        }
+        case 17:
+        {
+            alert("es de tarde");
+            break;
+        }
+        case 18:
+        {
+            alert("es de tarde");
+            break;
+        }
+        case 19:
+        {
+            alert("es de tarde");
+            break;
+        }
+        case 20:
+        {
+            alert("es de noche");
+            break;
+        }
+        case 21:
+        {
+            alert("es de noche");
+            break;
+        }
+        case 22:
+        {
+            alert("es de noche");
+            break;
+        }
+        case 23:
+        {
+            alert("es de noche");
+            break;
+        }
+        case 24:
+        {
+            alert("es de noche");
+            break;
+        }
+
+        }
+    }
+    else
+    {
+        alert("la hora no existe");  
+    }
+
+}//FIN DE LA FUNCIÓN
