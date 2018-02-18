80abdfa14c91d87f67d92288722b9bc9ab4d5170
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-18 19:22:28 -0300

    regalo para mamá
    estado: terminado.
    funciona: perfecto.
    observación: este programa es un regalo para mi mamá por su cumpleaños.

--- jsEntradaSalida-2.js
+++ jsEntradaSalida-2.js
@@ -2,13 +2,43 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	var nombre;
+	var amor;
 	
-	nombre=prompt("ingrese nombre","tomas friz");
-	alert("su nombre es: "  +nombre);
+	amor = prompt("Ingrese el nombre de la cumpliañera");
 
-
-	//var person = prompt("Please enter your name", "Harry Potter");
-
+	alert("Feliz cumple "  +amor);
 }
+/*
+ffffffffffffffffffff    eeeeeeeeeeeeeeeeeeee    lllllllll               iiiiiiiiii    zzzzzzzzzzzzzzzzzzzzz
+ffffffffffffffffffff    eeeeeeeeeeeeeeeeeeee    lllllllll               iiiiiiiiii    zzzzzzzzzzzzzzzzzzzzz
+fffffffff               eeeeeeee                lllllllll               iiiiiiiiii             zzzzzzzzzz
+fffffffff               eeeeeeee                lllllllll               iiiiiiiiii            zzzzzzzzzz
+ffffffffffffffffffff    eeeeeeeeeeeeeeeeeeee    lllllllll               iiiiiiiiii           zzzzzzzzzz
+ffffffffffffffffffff    eeeeeeeeeeeeeeeeeeee    lllllllll               iiiiiiiiii          zzzzzzzzzz
+fffffffff               eeeeeeee                lllllllll               iiiiiiiiii        zzzzzzzzzz
+fffffffff               eeeeeeee                lllllllll               iiiiiiiiii      zzzzzzzzzz
+fffffffff               eeeeeeeeeeeeeeeeeeee    lllllllllllllllllllll   iiiiiiiiii    zzzzzzzzzzzzzzzzzzzzz
+fffffffff               eeeeeeeeeeeeeeeeeeee    lllllllllllllllllllll   iiiiiiiiii    zzzzzzzzzzzzzzzzzzzzz
 
+ccccccccccccccccccc     uuuuuuuu     uuuuuuuu   mmmmmmm mmm   mmm mmmmmmm   pppppppppppppppppppppp    lllllllll                eeeeeeeeeeeeeeeeeeee 
+ccccccccccccccccccc     uuuuuuuu     uuuuuuuu   mmmmmmm mmm   mmm mmmmmmm   pppppppppppppppppppppp    lllllllll                eeeeeeeeeeeeeeeeeeee
+ccccccccc               uuuuuuuu     uuuuuuuu   mmmmmmm mmm   mmm mmmmmmm   ppppppp         pppppp    lllllllll                eeeeeeee
+ccccccccc               uuuuuuuu     uuuuuuuu   mmmmmmm  mmm mmm  mmmmmmm   ppppppp         pppppp    lllllllll                eeeeeeee
+ccccccccc               uuuuuuuu     uuuuuuuu   mmmmmmm   mmmmm   mmmmmmm   pppppppppppppppppppppp    lllllllll                eeeeeeeeeeeeeeeeeeee
+ccccccccc               uuuuuuuu     uuuuuuuu   mmmmmmm    mmm    mmmmmmm   pppppppppppppppppppppp    lllllllll                eeeeeeeeeeeeeeeeeeee
+ccccccccc               uuuuuuuu     uuuuuuuu   mmmmmmm           mmmmmmm   ppppppp                   lllllllll                eeeeeeee
+ccccccccc               uuuuuuuu     uuuuuuuu   mmmmmmm           mmmmmmm   ppppppp                   lllllllll                eeeeeeee
+ccccccccccccccccccc     uuuuuuuuuuuuuuuuuuuuu   mmmmmmm           mmmmmmm   ppppppp                   lllllllllllllllllllll    eeeeeeeeeeeeeeeeeeee
+ccccccccccccccccccc     uuuuuuuuuuuuuuuuuuuuu   mmmmmmm           mmmmmmm   ppppppp                   lllllllllllllllllllll    eeeeeeeeeeeeeeeeeeee
+
+mmmmmmm mmm   mmm mmmmmmm    aaaaaaaaaaaaaaaaaaa    mmmmmmm mmm   mmm mmmmmmm    aaaaaaaaaaaaaaaaaaa
+mmmmmmm mmm   mmm mmmmmmm    aaaaaa       aaaaaa    mmmmmmm mmm   mmm mmmmmmm    aaaaaa       aaaaaa
+mmmmmmm mmm   mmm mmmmmmm    aaaaaa       aaaaaa    mmmmmmm mmm   mmm mmmmmmm    aaaaaa       aaaaaa    
+mmmmmmm  mmm mmm  mmmmmmm    aaaaaa       aaaaaa    mmmmmmm  mmm mmm  mmmmmmm    aaaaaa       aaaaaa
+mmmmmmm   mmmmm   mmmmmmm    aaaaaaaaaaaaaaaaaaa    mmmmmmm   mmmmm   mmmmmmm    aaaaaaaaaaaaaaaaaaa    
+mmmmmmm    mmm    mmmmmmm    aaaaaaaaaaaaaaaaaaa    mmmmmmm    mmm    mmmmmmm    aaaaaaaaaaaaaaaaaaa     
+mmmmmmm           mmmmmmm    aaaaaa       aaaaaa    mmmmmmm           mmmmmmm    aaaaaa       aaaaaa    
+mmmmmmm           mmmmmmm    aaaaaa       aaaaaa    mmmmmmm           mmmmmmm    aaaaaa       aaaaaa
+mmmmmmm           mmmmmmm    aaaaaa       aaaaaa    mmmmmmm           mmmmmmm    aaaaaa       aaaaaa 
+mmmmmmm           mmmmmmm    aaaaaa       aaaaaa    mmmmmmm           mmmmmmm    aaaaaa       aaaaaa    
+*/   
