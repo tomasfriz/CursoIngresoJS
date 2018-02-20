49e1165d290efc9e419cb1e1c9d4f82f9a3f0720
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-20 09:43:55 -0300

    ejercicio 1 de while
    funciona: perfecto.
    se uso: while, document.write y "<br>".
    estado: terminado.

---
+++ jsIteraciones (1).js
@@ -0,0 +1,14 @@
+function Mostrar()
+{
+	var contador;
+    contador = 1;
+
+    while( contador < 10)
+    {
+
+	document.write("<br> " +contador);
+	contador++;
+    
+    }
+
+}//FIN DE LA FUNCIÓN
