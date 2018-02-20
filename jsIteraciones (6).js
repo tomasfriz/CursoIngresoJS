cf75ec56a9811d21efe84f876876964e54848447
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-20 11:41:41 -0300

    ejercicio 6 de while
    funciona: perfecto.
    se uso: while, prompt, parseInt y document.getElementById.
    estado: terminado.

---
+++ jsIteraciones (6).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+	var contador = 0;
+	var acumulador = 0;
+	var numero;
+	var promedio;
+	var iteraciones = 5;
+	
+	while(contador<iteraciones)
+	{
+		contador++
+		numero = prompt("ingrese un numero.");
+		numero = parseInt(numero);
+		acumulador = acumulador + numero;
+	}
+	promedio = acumulador / contador;
+
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = promedio;
+
+}//FIN DE LA FUNCIÓN
