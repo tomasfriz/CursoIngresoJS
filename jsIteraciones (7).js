42f14f61336820576abd79e80af466f1559594d6
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-21 15:40:00 -0300

    ejercicio 7 de while
    estado: no terminado.
    observación: faltan códigos, necesito ayuda del profesor.
    se generan bucles infinitos y no los puedo parar.
    la solución para detenerlo es cerrando la pagina.

--- jsIteraciones (7).js
+++ jsIteraciones (7).js
@@ -3,19 +3,23 @@ function Mostrar()
 
 	var contador = 0;
 	var acumulador = 0;
-	var respuesta ='si';
+	var respuesta;
 	var numero;
 	var promedio;
 
-	while(respuesta = "si")
+	while(respuesta = 'si')
 		{
 			contador++;
-			numero = prompt("ingrese un numero");
+			numero = prompt("ingrese un numero.");
 			numero = parseInt(numero);
-			acumulador = acumulador + numero;
-			respuesta = prompt("ingrese si para continuar");
-		}
-
+		    respuesta = prompt("ingrese 'si' para continuar");
+		
+		    if(respuesta = 'no')
+		    {
+				numero = parseInt(numero);
+				acumulador = acumulador + numero;
+		    }
+	    }
 		promedio = acumulador / contador;
 
 		document.getElementById('suma').value = acumulador;
