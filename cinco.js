95e0231b3676480cf4f9bb6f34b79994955afef9
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-21 16:37:48 -0300

    ejercicio 5 del parcial 1
    funciona: perfecto.
    se uso: prompt, switch, case, alert y break.
    estado: terminado.

---
+++ cinco.js
@@ -0,0 +1,28 @@
+function Mostrar()
+{
+    var DiaDeLaSemana;
+
+    DiaDeLaSemana = prompt("ingrese dia de la semana");
+
+    switch(DiaDeLaSemana)
+    {
+        case "lunes":
+
+        case "martes":
+
+        case "miercoles":
+
+        case "jueves":
+
+        case "viernes":
+        alert("a trabajar!!!");
+        break;
+
+        case "sabado":
+
+        case "domingo":
+        alert("es fin de semana");
+
+    }
+
+}
