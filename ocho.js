dc8da2eec3260f53d4fb6a6c723c1af3f7e235cf
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-24 12:29:53 -0300

    ejercicio 8 del parcial
    ejercicios echos por el profe
    no terminados

--- ocho.js
+++ ocho.js
@@ -1,22 +1,21 @@
 function Mostrar()
 {
- var nombre;
+    var nombre;
     var cantidad=0;
     var sexo;
-    sexo=f;
-    sexo=m;
+    var continuar = "";
 
-    while( sexo = "f" || sexo = "m" )
+    while(continuar != "no")
     {
         nombre = prompt("ingrese nombre");
         cantidad = cantidad +1;
-        if(sexo = masculino)
+        if(sexo = m)
         {
             alert("es masculino");
         }
         if(sexo = femenino)
         {
-            alert("es femenino");
+            alert("es f");
         }
     }
 }
