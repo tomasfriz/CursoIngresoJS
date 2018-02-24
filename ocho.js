a487b7881f06564e2da373fe3102744cb7b27d88
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-24 11:03:09 -0300

    ejercicio 6 y 8 del parcial 1

---
+++ ocho.js
@@ -0,0 +1,45 @@
+/* ingreso de alumnos, no sabemos cuantos hay, pedimos nombres, cuantos alumnos ingrese.
+*/
+function Mostrar()
+{
+    
+   /* var nombres;
+    var cantidad;
+    var nota = 0;
+    var acumulador = 0;
+    var promedio;
+    var continuar = "";
+
+    while(continuar != "no")
+    {
+        nombres = prompt("ingrese nombre");
+        cantidad = cantidad +1;
+        nota = prompt("ingrese nota");
+        nota = parseInt(nota);
+        while(nota<0 || nota>10)
+        {
+             nota = prompt("ingrese nota");
+             nota = parseInt(nota);
+        }
+        acumulador = acumulador + nota;
+        continuar = prompt("no, para continuar");
+    }
+    promedio = acumulador / nota;
+    document.write("la cantidad de alumnos es: " +cantidad);
+    document.write("el promedio de las notas es: " +promedio);
+    */
+
+
+    /*var nombres;
+    var cantidad = 0;
+    var seguir= "";
+
+    while(seguir = "NO")
+    {
+        nombres = prompt("ingrese los nombres de los alumnos");
+        cantidad = cantidad +1;
+        seguir = prompt("NO, para continuar");
+    }
+    seguir = document.write("los nombres son: " +seguir);
+*/
+}
---
+++ seis.js
@@ -0,0 +1,14 @@
+function Mostrar()
+{
+    var importes;
+    importe > 0;
+    importes = documnet.getElementById("importeFinal").value;
+
+    while(importe < 0)
+    {
+        importe = prompt("ingrese importe de venta");
+        
+    }
+
+
+}
