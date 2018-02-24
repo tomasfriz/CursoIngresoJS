010d07a9da521afda4f122bc850a57d3ea6aeb88
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-24 12:55:12 -0300

    ejercicio 8 del parcial 1
    falta terminar
    ejercicios echos por el profe.

--- ocho.js
+++ ocho.js
@@ -1,24 +1,27 @@
-function Mostrar()
+function Mostrar() 
 {
+
     var nombre;
-    var cantidad=0;
+    var cantidad = 0;
     var sexo;
     var continuar = "si";
     sexo = "m";
     sexo = "f";
 
-    while(continuar != "no")
+    while (continuar != "no") 
     {
         nombre = prompt("ingrese nombre");
-        cantidad = cantidad +1;
+        cantidad = cantidad + 1;
         sexo = prompt("ingrese sexo");
-        if(sexo = "m")
+
+        if (sexo = "m")
         {
             alert("es masculino");
         }
-        if(sexo = "f")
+        if (sexo = "f")
         {
             alert("es femenino");
         }
+         continuar = prompt("no, para continuar");
     }
 }
