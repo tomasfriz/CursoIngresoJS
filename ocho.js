201b0ea621cb2fe796824ae7cb478f700f25f29b
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-24 12:19:39 -0300

    ejerciocio 8 del parcial 1
    ejercicios echos por el profesor.

--- ocho.js
+++ ocho.js
@@ -1,45 +1,22 @@
-/* ingreso de alumnos, no sabemos cuantos hay, pedimos nombres, cuantos alumnos ingrese.
-*/
 function Mostrar()
 {
-    
-   /* var nombres;
-    var cantidad;
-    var nota = 0;
-    var acumulador = 0;
-    var promedio;
-    var continuar = "";
+ var nombre;
+    var cantidad=0;
+    var sexo;
+    sexo=f;
+    sexo=m;
 
-    while(continuar != "no")
+    while( sexo = "f" || sexo = "m" )
     {
-        nombres = prompt("ingrese nombre");
+        nombre = prompt("ingrese nombre");
         cantidad = cantidad +1;
-        nota = prompt("ingrese nota");
-        nota = parseInt(nota);
-        while(nota<0 || nota>10)
+        if(sexo = masculino)
         {
-             nota = prompt("ingrese nota");
-             nota = parseInt(nota);
+            alert("es masculino");
         }
-        acumulador = acumulador + nota;
-        continuar = prompt("no, para continuar");
+        if(sexo = femenino)
+        {
+            alert("es femenino");
+        }
     }
-    promedio = acumulador / nota;
-    document.write("la cantidad de alumnos es: " +cantidad);
-    document.write("el promedio de las notas es: " +promedio);
-    */
-
-
-    /*var nombres;
-    var cantidad = 0;
-    var seguir= "";
-
-    while(seguir = "NO")
-    {
-        nombres = prompt("ingrese los nombres de los alumnos");
-        cantidad = cantidad +1;
-        seguir = prompt("NO, para continuar");
-    }
-    seguir = document.write("los nombres son: " +seguir);
-*/
 }
