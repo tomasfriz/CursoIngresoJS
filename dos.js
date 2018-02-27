a968159425494650ae8245e4cff79e8ab189ecbf
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-27 17:12:43 -0300

    ejercicio 2 del recuperatorio
    funciona: perfecto.
    se uso: prompt, parseInt y document.getElementById.
    estado: terminado.

--- dos.js
+++ dos.js
@@ -1,22 +1,17 @@
-function Mostrar()
+function Mostrar() 
 {
-    var descuento
     var importe;
     var importeFinal;
 
-    importe = prompt("ingrese el precio del producto");
-    descuento = prompt("ingrese el porcentaje del descuento.");
+    importe = prompt("ingrese el importe del producto");
 
     importe = parseInt(importe);
-    descuento = parseInt(descuento);
 
-    importeFinal = importe * descuento;
+    importeFinal = importe * 0.25;
 
     importeFinal = parseInt(importeFinal);
 
-    importeFinal = importe + importeFinal;
+    importeFinal = importe - importeFinal;
 
     document.getElementById("importeFinal").value = importeFinal;
-
-  
 }
