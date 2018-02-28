c262d46702f368330e21744f36f7594b40b6324f
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-27 23:10:38 -0300

    ejercicio 8 del parcial 3
    funciona: no.
    estado: no terminado.
    observación:
    - faltan códigos en el programa.
    - esta hecho un parte del programa.

---
+++ 8-iteraciones.js
@@ -0,0 +1,21 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostrar()
+{
+	var numerosPositivos=0;
+	var promedio;
+	var contadorPares=0;
+	var sumarNumeros;
+	var maximo;
+	var minimo;
+	
+	while(true)
+	{
+		numerosPositivos = prompt("ingrese un numero positivo");
+		if(numerosPositivos==null)
+		{
+			break;
+		}
+		
+	}
+}
+
