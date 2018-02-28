e3698edda9fe5daf4ec83aa043322e47ac45e83a
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-02-27 23:03:48 -0300

    ejercicio 7 del parcial 3
    funciona: no
    estado: no terminado.
    observación:
    - faltan códigos en el programa.
    - es una parte del programa.

---
+++ 7-iteraciones.js
@@ -0,0 +1,16 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostrar()
+{
+	var notas;
+	var sexo;
+	sexo="f";
+	sexo="m";
+	var interacciones=6;
+	var alumnos;
+
+	while(alumnos<interacciones)
+	{
+		
+	}
+}
+
