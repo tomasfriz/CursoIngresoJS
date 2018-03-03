c4ac8294feeff3a470967cc81cc82536888352b4
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-03-03 18:26:56 -0300

    ejercicio 3 de for
    funciona: perfecto.
    estado: terminado.

---
+++ jsIteracionesFor (3).js
@@ -0,0 +1,13 @@
+function Mostrar()
+{
+    var repeticiones = prompt("ingrese el número de repeticiones");
+    for(var i = 0;repeticiones; repeticiones--)
+    {
+        alert("hola UTN FRA");
+    }
+    
+
+
+
+
+}//FIN DE LA FUNCIÓN
