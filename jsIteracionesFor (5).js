1f7d55b4b52a044e38b7c214c7b729ddafd7eec8
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2018-03-04 15:09:31 -0300

    ejercicio 5 de for
    funciona: perfecto.
    estado: terminado.

---
+++ jsIteracionesFor (5).js
@@ -0,0 +1,15 @@
+function Mostrar()
+{
+    var numero;
+    numero = prompt("ingrese el numero nueve");
+    numero = parseInt(numero);
+    for(; numero != 9;)
+    {
+        numero = prompt("error, ingrese el numero nueve");
+        numero = parseInt(numero);
+    }
+
+
+
+
+}//FIN DE LA FUNCIÓN
